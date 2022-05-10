d02f362dd8fd4aca130669a5c13fcfa32724b9f4
Xuan Li <Xuan.Li@etu.univ-savoie.fr>
2022-05-10 19:52:06 +0200

    For Tutorial

---
+++ src/Interface/ScrollableLabel.java
@@ -0,0 +1,38 @@
+package Interface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ScrollableLabel extends JLabel implements Scrollable {
+
+    public ScrollableLabel(ImageIcon i) {
+        super(i);
+    }
+
+    public Dimension getPreferredScrollableViewportSize() {
+        return getPreferredSize();
+
+    }
+
+    @Override
+    public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+        return 10;
+    }
+
+    public int getScrollableBlockIncrement(Rectangle r, int orietation, int direction) {
+        return 10;
+
+    }
+
+    public boolean getScrollableTracksViewportHeight() {
+        return false;
+
+    }
+
+    public boolean getScrollableTracksViewportWidth() {
+        return false;
+
+    }
+
+
+}
