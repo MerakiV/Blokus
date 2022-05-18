269dc5f8468022b2697f96ffb60d2b94fdfa1fe9
Hugo R <55627173+IHUN-ihateusernames@users.noreply.github.com>
2022-05-18 15:36:23 +0200

    Completed Placements methods for ControllerAIMinMax

--- src/Controller/ControllerAIMinMax.java
+++ src/Controller/ControllerAIMinMax.java
@@ -3,10 +3,7 @@ package Controller;
 import Players.Player;
 import Structures.*;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class ControllerAIMinMax {
 
@@ -95,36 +92,129 @@ public class ControllerAIMinMax {
     public static int evaluation(Game config, boolean max){
         //(our score - opponent score) + (our possible placements - opponent placements)
 
-        int p1score1 = config.getPlayerList().get(0).getScore();
-        int p1score2 = config.getPlayerList().get(3).getScore();
-        int p2score1 = config.getPlayerList().get(1).getScore();
-        int p2score2 = config.getPlayerList().get(2).getScore();
+        Player p1c1 = config.getPlayerList().get(0);
+        Player p1c2 = config.getPlayerList().get(2);
+        Player p2c1 = config.getPlayerList().get(1);
+        Player p2c2 = config.getPlayerList().get(3);
+
+        int p1score1 = p1c1.getScore();
+        int p1score2 = p1c2.getScore();
+        int p2score1 = p2c1.getScore();
+        int p2score2 = p2c2.getScore();
+
+        int sumScoreP1 = p1score1 + p1score2;
+        int sumScoreP2 = p2score1 - p2score2;
+        int sumPlacementsP1 = sumAllPlacements(p1c1) + sumAllPlacements(p1c2);
+        int sumPlacementsP2 = sumAllPlacements(p2c2) + sumAllPlacements(p2c2);
 
         if(max){
-            return (p1score1 + p1score2 - p2score1 - p2score2); //+ possible placements
+            return (sumScoreP1 - sumScoreP2) + (sumPlacementsP1 - sumPlacementsP2);
         }
         else{
-            return (p2score1 + p2score2 - p1score1 - p1score1);   //+ possible placements
+            return (sumScoreP2 - sumScoreP1) + (sumPlacementsP2 - sumPlacementsP1);
         }
     }
 
-    // returns a list of every pieces that can be put for a given corner
-    public List<Piece> allPlacementsforCorner(List<Piece> lp, Color color, int x, int y){
-        List<Piece> listPlacements = new ArrayList<>();
-        //see for every shape
-        //int nbcorners = board.getCorner(color);
+    // returns a list of every shapes that can be put for a given corner (unused rn)
+    public Hashtable<Shape, List<Tile>> allPlacementsforCorner(List<Piece> lp, Color color, int x, int y){
+        Hashtable<Shape, List<Tile>> listPlacements = new Hashtable<>();
+        Board b = config.getBoard();
+        Iterator<Tile> it;
+        List<Tile> lt;
+        Tile t1, t2;
+        int k, ax, ay;
         for(int ip= 0; ip <= lp.size(); ip++) {
             Piece p = lp.get(ip);
-            if (config.getBoard().canPut(p, color, x, y)) {
-                listPlacements.add(p);
+            List<Shape> ls = p.getShapeList();
+            for(Shape s: ls) {
+                lt = new ArrayList<>();
+                for (k=0; k<4; k++) {
+                    it = s.getCornerList(k).iterator();
+                    while(it.hasNext()) {
+                        t1 = it.next();
+                        ax = x - t1.getX() + s.getAnchorX();
+                        ay = y - t1.getY() + s.getAnchorY();
+                        if (b.canPut(s, color, ax, ay)) {
+                            t2 = new Tile(ax, ay);
+                            lt.add(t2);
+                        }
+                    }
+                }
+                if(!lt.isEmpty()){
+                    listPlacements.put(s, lt);
+                }
             }
         }
         return listPlacements;
     }
 
-    // returns a list of every pieces that can be put for every corners
-    public List<List<Piece>> allPlacements(List<Piece> lp, Color color){
-        //availableCorners
-        return null;
+    // returns a list of every pieces that can be put for every corners (unused rn)
+    public Hashtable<Shape, List<Tile>> allPlacements(List<Piece> lp, Color color){
+        Hashtable<Shape, List<Tile>> listPlacements = new Hashtable<>();
+        Board b = config.getBoard();
+        Iterator<Tile> it1, it2;
+        List<Tile> lt;
+        Tile t1, t2, t3;
+        int k, ax, ay;
+        for(int ip= 0; ip <= lp.size(); ip++) {
+            Piece p = lp.get(ip);
+            List<Shape> ls = p.getShapeList();
+            for(Shape s: ls) {
+                lt = new ArrayList<>();
+                for (k=0; k<4; k++) {
+                    it1 = s.getCornerList(k).iterator();
+                    while(it1.hasNext()) {
+                        t1 = it1.next();
+                        it2 = b.getCorners(color, k).iterator();
+                        while (it2.hasNext()) {
+                            t2 = it2.next();
+                            ax = t2.getX() - t1.getX() + s.getAnchorX();
+                            ay = t2.getY() - t1.getY() + s.getAnchorY();
+                            if (b.canPut(s, color, ax, ay)) {
+                                t3 = new Tile(ax, ay);
+                                lt.add(t3);
+                            }
+                        }
+                    }
+                }
+                if(!lt.isEmpty()){
+                    listPlacements.put(s, lt);
+                }
+            }
+        }
+        return listPlacements;
     }
+
+    int sumAllPlacements(Player p){
+        int sum = 0;
+        List<Piece> lp = p.getPieces();
+        Color color = p.getColor();
+        Board b = config.getBoard();
+        Iterator<Tile> it1, it2;
+        Tile t1, t2;
+        int k, ax, ay;
+        for(int ip= 0; ip <= lp.size(); ip++) {
+            Piece p = lp.get(ip);
+            List<Shape> ls = p.getShapeList();
+            for(Shape s: ls) {
+                for (k=0; k<4; k++) {
+                    it1 = s.getCornerList(k).iterator();
+                    while(it1.hasNext()) {
+                        t1 = it1.next();
+                        it2 = b.getCorners(color, k).iterator();
+                        while (it2.hasNext()) {
+                            t2 = it2.next();
+                            ax = t2.getX() - t1.getX() + s.getAnchorX();
+                            ay = t2.getY() - t1.getY() + s.getAnchorY();
+                            if (b.canPut(s, color, ax, ay)) {
+                                sum++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+
 }
