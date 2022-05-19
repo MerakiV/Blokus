553d4b3df2c3a25c187a2327b61385e55022be5f
ancilloy <95426229+ancilloy@users.noreply.github.com>
2022-05-19 09:43:39 +0200

    Add files via upload

--- src/Controller/ComparatorAIMinMax.java
+++ src/Controller/ComparatorAIMinMax.java
@@ -5,14 +5,16 @@ import java.util.Comparator;
 import Structures.Game;
 
 public class ComparatorAIMinMax implements Comparator<Game> {
+    ControllerAIMinMax ctrl;
     boolean max;
 
-    public ComparatorAIMinMax(boolean mx) {
+    public ComparatorAIMinMax(ControllerAIMinMax ctl, boolean mx) {
+        ctrl = ctl;
         max = mx;
     }
 
     @Override
     public int compare(Game g1, Game g2) {
-        return ControllerAIMinMax.evaluation(g1, max) - ControllerAIMinMax.evaluation(g2, max);
+        return ctrl.evaluation(g1, max) - ctrl.evaluation(g2, max);
     }
 }
