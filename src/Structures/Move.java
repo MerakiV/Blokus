be03f335f647d662ce2d9a55283377ad59519d7a
Akira Tobing <akira.tobing@kanisius.org>
2022-05-23 15:56:00 +0200

    Added Move class

---
+++ src/Structures/Move.java
@@ -0,0 +1,27 @@
+package Structures;
+
+public class Move {
+    Piece piece;
+    Tile tile;
+
+    public Move(Piece p, Tile t){
+        this.piece = p;
+        this.tile = t;
+    }
+
+    public Piece getPiece(){
+        return piece;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
+}
