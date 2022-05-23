2fc2856bad40cf50bbb9ff056d38f39b7239f5c3
Akira Tobing <akira.tobing@kanisius.org>
2022-05-23 11:02:22 +0200

    Added score to Player2P

--- src/Players/PlayerHuman2P.java
+++ src/Players/PlayerHuman2P.java
@@ -15,6 +15,7 @@ public class PlayerHuman2P extends Player2P {
     public PlayerHuman2P(Color col1, Color col2){
         pcol1 = new PlayerHuman(col1);
         pcol2 = new PlayerHuman(col2);
+        score = pcol1.score + pcol2.score;
     }
 
 }
