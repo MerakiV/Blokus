8187188ef8f8472c1fda23d01481765dcb6ed0d3
MerakiV <kimmyga01@gmail.com>
2022-05-29 12:06:12 +0200

    Interface - added ability for piece orientation with mouse

---
+++ src/Interface/WheelEvent.java
@@ -0,0 +1,31 @@
+package Interface;
+
+import Controller.ControllerGamePlay;
+
+import javax.swing.*;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+public class WheelEvent implements MouseWheelListener {
+
+    ControllerGamePlay controller;
+
+    public WheelEvent(ControllerGamePlay c){
+        this.controller = c;
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        if (controller.piece != null){
+            if(e.getWheelRotation() < 0){
+                System.out.println("Mouse moved UP");
+                controller.command("counterclockwise");
+            } else {
+                System.out.println("Mouse moved DOWN");
+                controller.command("clockwise");
+            }
+        } else{
+            System.out.println("No piece selected");
+        }
+    }
+}
